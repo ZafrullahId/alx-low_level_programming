0d3d39d31fd88541a744a603e77f653ba87f37ca
ZafrullahId <108731105+ZafrullahId@users.noreply.github.com>
2022-12-12 22:19:32 +0100

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,9 +1,6 @@
 /*
-
  * File: 5-print_numbers.c
-
  * Auth: Brennan D Baraban
-
  */
 
 
@@ -13,13 +10,9 @@
 
 
 /**
-
  * main - Prints all single digit numbers of base 10 starting from 0.
-
  *
-
  * Return: Always 0.
-
  */
 
 int main(void)
@@ -28,18 +21,11 @@ int main(void)
 
 	int num;
 
-
-
 	for (num = 0; num < 10; num++)
 
 		printf("%d", num);
 
-
-
 	printf("\n");
 
-
-
 	return (0);
-
 }

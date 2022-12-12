34ea64d876c857c75e14d7783a95de9c8a45753c
ZafrullahId <108731105+ZafrullahId@users.noreply.github.com>
2022-12-12 13:46:06 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,7 +7,6 @@
 
 /**
 * main - Prints the alphabet in lowercase.
-* 
 *  Return: Always 0.
 */
 int main(void)
@@ -17,6 +16,5 @@ int main(void)
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 	putchar('\n');
-	
 	return (0);
 }

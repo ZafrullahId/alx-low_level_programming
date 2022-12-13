3be338474eba9daee9bee36cb0263715281136ac
ZafrullahId <108731105+ZafrullahId@users.noreply.github.com>
2022-12-13 16:09:31 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,8 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
-
   * main - Entry point
-
-  *
-
   * Return: Always 0
-
   */
 
 int main(void)
@@ -17,27 +10,15 @@ int main(void)
 {
 
 	_putchar('H');
-
 	_putchar('o');
-
 	_putchar('l');
-
 	_putchar('b');
-
 	_putchar('e');
-
 	_putchar('r');
-
 	_putchar('t');
-
 	_putchar('o');
-
 	_putchar('n');
-
 	_putchar('\n');
 
-
-
 	return (0);
-
 }

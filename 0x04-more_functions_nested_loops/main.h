2744188c71af740556c69b83883a09c921129a09
ZafrullahId <108731105+ZafrullahId@users.noreply.github.com>
2022-12-17 21:34:11 +0100

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,50 +1,27 @@
 #ifndef MAIN_H
-<<<<<<< HEAD
-
 #define MAIN_H
 
-
-
-
-
 int _putchar(char c);
-
 int _isupper(int c);
-
 int _isdigit(int c);
-
 int mul(int a, int b);
-
 void print_numbers(void);
-
 void print_most_numbers(void);
-
 void more_numbers(void);
-
 void print_line(int n);
-
 void print_diagonal(int n);
-
 void print_square(int size);
-
 void print_triangle(int size);
-
 void print_number(int n);
-
-
-
-
 
 #endif 
 
 /* 
-
 Bwave ICT 
 
 =======
 #define MAIN_H
 
-
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -57,7 +34,6 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-
 
 #endif 
 /* 

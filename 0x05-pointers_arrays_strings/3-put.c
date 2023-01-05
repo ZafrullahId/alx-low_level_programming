c27278977688becea240c9f0c6bd7730402aaaae
ZafrullahId <108731105+ZafrullahId@users.noreply.github.com>
2023-01-05 14:21:25 +0100

    Create 3-put.c

---
+++ 0x05-pointers_arrays_strings/3-put.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: the string
+ *
+ * Return: the length of the string
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privac
